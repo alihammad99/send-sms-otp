bafdf89f15eef9b63ef775209da27c65e6afe878
Ali Hammad <aliressan.ar@gmail.com>
2024-01-15 23:46:26 +0300

    first commit

---
+++ src/controllers/verify-otp.js
@@ -0,0 +1,1 @@
+import{verifyOTPCode}from"../middlewares/verify-otp.js";export const verifyOTP=(phoneNumber,otpCode,environment="development")=>verifyOTPCode(phoneNumber,otpCode,environment);
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export{sendMessage}from"./controllers/send-message.js";export{verifyOTP}from"./controllers/verify-otp.js";
